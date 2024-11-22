b3bc6276b839420c8f4d255e8f1c0ed9310cfe09
missiona.carla <missiona.carla@gmail.com>
2024-11-22 14:15:41 +0000

    asdf

--- src/main/java/com/tsaic/tshirtshop/webapp/WebappApplication.java
+++ src/main/java/com/tsaic/tshirtshop/webapp/WebappApplication.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@SpringBootApplication(exclude =  {DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class WebappApplication {
  
 	public static void main(String[] args) {
